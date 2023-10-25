26017a2d562e52b7769513b230a5627faf2d2921
willofD13 <leonidastsiakas13@gmail.com>
2023-10-25 16:17:39 +0300

    Fix status code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
     if @article.save
       redirect_to @article
     else
-      render :new, :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 end
